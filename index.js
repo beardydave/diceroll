444bba5fa82bc939d891fdd4733eefce12799eb7
beardydave <62890800+beardydave@users.noreply.github.com>
2020-04-07 18:44:33 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function diceRoll() {
   var randomNumber = Math.floor(Math.random() * 6) + 1;
   document.querySelector("h1").innerHTML="You rolled a "+randomNumber;
-  var phraseList = ["Sweet!","Awesome!","Well done!","Epic!"];
+  var phraseList = ["Sweet!","Awesome!","Well done!","Epic!","Great!","Nice one!"];
   var randomPhraseNumber = Math.floor(Math.random() * phraseList.length);
   var randomPhrase = phraseList[randomPhraseNumber];
   document.querySelector("h2").innerHTML=randomPhrase;
@@ -13,7 +13,7 @@ function twoDiceRoll() {
   var b = Math.floor(Math.random() * 6) + 1;
   document.querySelector("h1").innerHTML="You rolled a "+a+" and a "+b;
   document.querySelector("h2").innerHTML="Sweet!";
-  var phraseList = ["Sweet!","Awesome!","Well done!","Epic!"];
+  var phraseList = ["Sweet!","Awesome!","Well done!","Epic!","Great!","Nice one!"];
   var randomPhraseNumber = Math.floor(Math.random() * phraseList.length);
   var randomPhrase = phraseList[randomPhraseNumber];
   document.querySelector("h2").innerHTML=randomPhrase;
